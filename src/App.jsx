0177b5662c0b57fdaccb574de19783651a689287
helloservices00-blip <helloservices00@gmail.com>
2025-11-24 01:22:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
+// Pages
 import ModulesPage from "./pages/ModulesPage";
-import VendorsPage from "./pages/VendorsPage";
-import CategoriesPage from "./pages/CategoriesPage";
-import SubcategoriesPage from "./pages/SubcategoriesPage";
-import ProductsPage from "./pages/ProductsPage";
+// Later you can add VendorsPage, CategoriesPage, etc.
 
 function App() {
   return (
@@ -13,10 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<ModulesPage />} />
         <Route path="/modules" element={<ModulesPage />} />
-        <Route path="/modules/:moduleId/vendors" element={<VendorsPage />} />
-        <Route path="/vendors/:vendorId/categories" element={<CategoriesPage />} />
-        <Route path="/categories/:categoryId/subcategories" element={<SubcategoriesPage />} />
-        <Route path="/subcategories/:subcategoryId/products" element={<ProductsPage />} />
       </Routes>
     </Router>
   );
