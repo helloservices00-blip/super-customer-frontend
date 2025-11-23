c528fbd5a4b023742eff72a5d18780abb6323a48
helloservices00-blip <helloservices00@gmail.com>
2025-11-23 20:25:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Profile from "./pages/Profile";
-import Products from "./pages/Products";
+import React, { useState } from "react";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
+  const [showLogin, setShowLogin] = useState(true);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
-    </Router>
+    <div style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      background: "#f0f2f5"
+    }}>
+      {showLogin ? (
+        <LoginPage switchToSignup={() => setShowLogin(false)} />
+      ) : (
+        <SignupPage switchToLogin={() => setShowLogin(true)} />
+      )}
+    </div>
   );
 }
 
