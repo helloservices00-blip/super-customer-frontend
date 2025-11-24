74aae6d27333146b7225c23ca317f611adb0105a
helloservices00-blip <helloservices00@gmail.com>
2025-11-24 07:18:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-
-// Pages
 import ModulesPage from "./pages/ModulesPage";
-// Later you can add VendorsPage, CategoriesPage, etc.
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ModulesPage />} />
-        <Route path="/modules" element={<ModulesPage />} />
       </Routes>
     </Router>
   );
